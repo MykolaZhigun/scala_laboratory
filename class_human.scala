306c97bdd21a260315c587374c2dc268fe16310d
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-10-09 12:14:45 +0300

    Update class_human.scala

--- class_human.scala
+++ class_human.scala
@@ -1,16 +1,17 @@
-class Human(name: String, age: Int, height: Int, weight: Int, country_live: String, region_live: String) {
+class Human(name: String, age: Int, data_birth: String, height: Int, weight: Int, country_live: String, region_live: String) {
   val fullName: String = name
   val personAge: Int = age
+  val birthday: String = data_birth
   val height_human: Int = height
   val weight_human: Int = weight
   val country: String = country_live
   val region: String = region_live
 
-  override def toString: String = s"Name: $fullName\nAge: $personAge\nCountry: $country\nRegion: $region\nHeight: $height_human\nWeight: $weight_human"
+  override def toString: String = s"Name: $fullName\nAge: $personAge\nBirthday: $birthday\nCountry: $country\nRegion: $region\nHeight: $height_human\nWeight: $weight_human"
 }
 
 object Main extends App {
-  val name_people = new Human("Mykola", 18, height = 179, weight = 53, country_live = "Ukraine", region_live = "Kherson region")
+  val name_people = new Human("Mykola", 18, data_birth = "23.09.2005", height = 179, weight = 53, country_live = "Ukraine", region_live = "Kherson region")
 
   println(name_people)
 }
